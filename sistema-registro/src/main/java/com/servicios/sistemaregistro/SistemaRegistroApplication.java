636e4b17a1889a79cb9a0efa7d45abf1941eb407
kysssh <kyshze10@gmail.com>
2026-06-26 01:16:34 -0500

    feat: configuración inicial del proyecto Spring Boot con conexión a PostgreSQL

---
+++ sistema-registro/src/main/java/com/servicios/sistemaregistro/SistemaRegistroApplication.java
@@ -0,0 +1,13 @@
+package com.servicios.sistemaregistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRegistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRegistroApplication.class, args);
+	}
+
+}
